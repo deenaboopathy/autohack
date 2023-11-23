32a4fec93f3cbd4e7a6f93977589f14d52223f69
arjun-talpula <151831693+arjun-talpula@users.noreply.github.com>
2023-11-23 11:01:00 +0000

    response check

--- automationscript.py
+++ automationscript.py
@@ -1,3 +1,11 @@
 # test file
 
 print ( " hello world")
+import requests
+r = requests.get("http://34.105.137.169:8070/")
+print(str(r.status_code) + '\n')
+print(str(r.content) + '\n')
+if("Hello, Docker Version Two" in str(r.content )):
+    print("Sucess "+ '\n')
+else:
+    print("failure")
