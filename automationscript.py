f8bc1abe036aaa526d9e1b7dbe6130d42d0bc82a
Deena Dhayalan Boopathy <123478478+deenaboopathy@users.noreply.github.com>
2023-11-23 13:37:25 +0000

    Update automationscript.py

--- automationscript.py
+++ automationscript.py
@@ -2,6 +2,7 @@
 
 print ( " hello world")
 import requests
+import sys
 r = requests.get("http://34.105.137.169:8070/")
 print(str(r.status_code) + '\n')
 print(str(r.content) + '\n')
@@ -9,3 +10,4 @@ if("Hello, Docker Version Two" in str(r.content )):
     print("Sucess "+ '\n')
 else:
     print("failure")
+    sys.exit(-1)
